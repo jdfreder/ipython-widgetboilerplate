e791d25db26fc31ae4b58644e6aa6d6197c53f8e
cekees <cekees@gmail.com>
2015-05-13 13:07:59 -0500

    derived from DOMWidget instead of Box

--- mywidget/mywidget.py
+++ mywidget/mywidget.py
@@ -1,8 +1,7 @@
-from IPython.html.widgets import DOMWidget,Box
+from IPython.html.widgets import DOMWidget
 from IPython.utils.traitlets import Unicode, Int
-from IPython.utils.traitlets import Int
 
-class MyWidget(Box):
+class MyWidget(DOMWidget):
     _view_module = Unicode('nbextensions/mywidget/mywidget', sync=True)
     _view_name = Unicode('MyWidgetView', sync=True)
     count = Int(sync=True)
